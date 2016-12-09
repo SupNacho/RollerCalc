b4dff9a3802768a5d160dc4506c7e92179b4c93f
SuperNacho <vanbellum@gmail.com>
2016-12-09 12:16:21 +0300

    Добавлены иконки, переработан горизонтальный вид, добавлено описание о программе

--- app/src/main/java/ru/supernacho/rollercalc/AbActivity.java
+++ app/src/main/java/ru/supernacho/rollercalc/AbActivity.java
@@ -31,6 +31,7 @@ public class AbActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
     }
